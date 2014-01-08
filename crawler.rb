f9722248d5858fe5179333713e6f0ecca94b1a67
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-08 15:20:55 -0600

    Switching url for ebay

--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,10 @@
-require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
 #Get a Nokogiri::HTML::Document for the page
 
-doc = Nokogiri::HTML(open('http://kwazarmultimedia.com/cityspa/bullshit.htm', 
-                          'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22'))
+doc = Nokogiri::HTML(open('http://www.ebay.com/itm/CHLOE-GREY-BOOTS-SIZE-38-5-/181292865028?pt=US_Women_s_Shoes&hash=item2a35e59604', 
+                          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36)
 
 # doc.css('h1').each do |title|
 #   puts title.content
