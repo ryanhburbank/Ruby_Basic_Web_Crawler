a4fb52146a5bb5740393b40de17241cea5d40f66
Andrew Jorczak <andrew.jorczak@gmail.com>
2014-01-08 16:02:58 -0600

    Finished scraping to hash

--- crawler.rb
+++ crawler.rb
@@ -1,12 +1,60 @@
 require 'nokogiri'
 require 'open-uri'
+require 'CSV'
 
 #Get a Nokogiri::HTML::Document for the page
 
-doc = Nokogiri::HTML(open('http://www.ebay.com/itm/CHLOE-GREY-BOOTS-SIZE-38-5-/181292865028?pt=US_Women_s_Shoes&hash=item2a35e59604', 
-                          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36)
+doc = Nokogiri::HTML(open('ebay.html', 
+                          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'))
 
-# doc.css('h1').each do |title|
-#   puts title.content
+
+# Class names to look for : nume_item_title, nume_price, nume_b_format span, 
+# 1. Capture all titles as an array.
+# 2. 
+
+
+# [Title: ?, Price: ?, Format: ?]
+#
+# 
+# 
+# 
+# 
+# 
+# 
+
+# class ItemInfo
+# 	def get_title
+# 	end
+
+# 	def get_price
+# 	end
+
+
 # end
-puts doc
+
+title_array = []
+price_array = []
+order_option = []
+item_array = []
+item = {}
+
+doc.css('.nume_item_title').each do |title|
+	title = title.text.gsub("\n",'')
+  title_array << title
+end
+doc.css('.nume_price').each do |price|
+	price = price.text.gsub("\n",'')
+	price_array << price
+end
+doc.css('.nume_b_format span').each do |order|
+	order = order.text.gsub("\n", '')
+	order_option << order
+end 
+
+(title_array.length).times do |i|
+	item = { :title => title_array[i],
+					 :price => price_array[i],
+					 :order_option => order_option[i]}
+	item_array << item
+end
+
